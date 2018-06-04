80325de029e7e930b96931ef0f7f42ba569642ad
codespool <sasa.pul@gmail.com>
2018-06-04 03:18:10 +0200

    add bulma and card component

---
+++ src/components/ToiletCard.component.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import glamorous from 'glamorous'
+
+export const ToiletCard = function ToiletCard ({type, waitingTime,queueLength, where}) {
+  return (
+    <Wrapper>
+      <p>
+        <strong>Type: </strong>
+        <span>{type}</span>
+      </p>
+      <p>
+        <strong>People waiting: </strong>
+        <span>{queueLength}</span>
+      </p><p>
+        <strong>Approx. waiting time: </strong>
+        <span>{waitingTime}</span>
+      </p><p>
+        <strong>Where is it: </strong>
+        <span>{where}</span>
+      </p>
+    </Wrapper>
+  )
+}
+
+const Wrapper = glamorous.div({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '1em',
+  margin: '1em',
+  border: '1px solid rgba(150, 150, 150, 0.5)',
+  borderRadius: '5px'
+})
